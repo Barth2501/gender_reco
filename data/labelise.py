ddd0bd50e5c099cf017b0277d9304d9ba9abd6d5
barth2501 <barthelemy.lancelot@student.ecp.fr>
2020-06-24 17:59:26 +0200

    ready to be add to the pipeline

--- data/labelise.py
+++ data/labelise.py
@@ -1,15 +1,19 @@
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
+from PIL import Image
+
 
 ds = tfds.load('celeb_a', split='train',shuffle_files=True)
 ds = ds.batch(1)
 count=1
 for example in tfds.as_numpy(ds):
     image, label = example["image"], example["attributes"]
+    image = Image.fromarray(image[0])
     if label['Male'][0]==True:
         image.save('./{}/male_{}.jpg'.format('male',count))
     else:
         image.save('./{}/female_{}.jpg'.format('female',count))
     count+=1
-    if count==2:
+    print(count)
+    if count==5000:
         break
